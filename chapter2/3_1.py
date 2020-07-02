19877866061d4cdb43cbe06050fa41c17d5a1dd1
zhuhonglinX <zhuhl1996@gmail.com>
2020-07-02 20:38:11 +0800

    chapter2 3.1

---
+++ chapter2/3_1.py
@@ -0,0 +1,22 @@
+"""
+
+"""
+
+def find_dup(lst):
+    
+    for i in range(len(lst)):
+        while lst[i] != i:
+            m = lst[i]
+
+            if lst[m] == lst[i]:
+                return m
+            
+            # swap number position i and m
+            lst[i], lst[m] = lst[m], lst[i]
+
+
+
+
+if __name__ == '__main__':
+    lst = [2, 3, 1, 0, 2, 5, 3]
+    print(find_dup(lst))
